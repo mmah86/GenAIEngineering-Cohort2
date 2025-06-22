f0e0393ebc708b852efc127a2e303b2751afd0b2
mmah86 <mmahesh@softclap.com>
2025-06-22 20:49:25 +0530

    Update calculator.py

--- BaseCamp3/Day_2/calculator.py
+++ BaseCamp3/Day_2/calculator.py
@@ -6,7 +6,8 @@ st.title("Calculator App")
 st.write("This app connects to a FastAPI calculator service.")
 
 # Define the API base URL
-api_url = "http://0.0.0.0:9321"
+# api_url = "http://0.0.0.0:9321"
+api_url = "https://genaiengineering-cohort2-txw2.onrender.com/"
 
 # Initialize session state to store the calculator display and current operation
 if 'display' not in st.session_state:
@@ -63,8 +64,9 @@ def calculate_result():
         endpoint = f"{api_url}/{st.session_state.operation}"
 
         # Make the API call
-        response = requests.get(endpoint, params={"a": first_num, "b": second_num})
-
+        # response = requests.get(endpoint, params={"a": first_num, "b": second_num})
+        response = requests.post(endpoint, params={"a": first_num, "b": second_num})
+        
         # Check if the request was successful
         if response.status_code == 200:
             result = response.json()
